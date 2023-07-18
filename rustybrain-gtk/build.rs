71b00a81efb0bd935ae54e82a4b7d774b1a538ec
wanghui <wh_linux@126.com>
2023-07-18 13:37:29 +0800

    feat: upgrade gio

--- rustybrain-gtk/build.rs
+++ rustybrain-gtk/build.rs
@@ -1,6 +1,10 @@
 fn main() {
     let output = "app.gresource";
-    gio::compile_resources("resources", "resources/app.gresource.xml", output);
+    glib_build_tools::compile_resources(
+        &["resources"],
+        "resources/app.gresource.xml",
+        output,
+    );
     println!("cargo:rerun-if-changed=resources/*");
     println!("cargo:rerun-if-changed=build.rs");
 }
